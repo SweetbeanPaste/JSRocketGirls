22dcbb94307980f52acc05c062ea4b74c42e82c7
Jenny King <jen.kin.bus@gmail.com>
2025-02-10 15:45:32 -0600

    Sorting Cards
    Player and PC desks fucntion ->readyBTN

--- ProyectoFinal/scripts/index.js
+++ ProyectoFinal/scripts/index.js
@@ -1,7 +1,7 @@
 //Creating characters from deck
 //# PROBLEM UNSOLVE: when switches tabs to "about" this process will be executed again
 const names = ["Allen","Dinara","Eliv","Urumi","Clith","Dansu"];
-const charactersDeck = [];
+let charactersDeck = [];
 
 document.addEventListener('DOMContentLoaded', function() {   
 let section = document.getElementsByClassName('carddeck_Section'); 
@@ -26,9 +26,10 @@ for (i = 0 ; i<=5 ;i++) {
     section[0].appendChild(newCard);
     
     charactersDeck.push(character);
-    console.log(character); 
+    createClickListener(newCard,character);
+    //console.log(character); 
 } 
-console.log(charactersDeck);
+//console.log(charactersDeck);
 })
 
 function getRandom() {
@@ -36,3 +37,71 @@ function getRandom() {
     let number =Math.floor(Math.random() * 11);
     return number;
   }
+
+// Click Func to modify and Delete
+function createClickListener(newCard,character)
+{
+    newCard.addEventListener("click",function(){
+        //removes the click Card from the Deck
+        updateMainDeck(character);
+        let playerDeck = document.getElementById("currentPYdeck");
+
+        // Check if the player already has 3 cards
+        if (playerDeck.children.length < 3) {
+            // Remove card from main deck
+            newCard.remove();
+
+            // Create a new card for the player's deck
+            let selectedCard = document.createElement("img");
+            selectedCard.src = character.imgurl;
+            selectedCard.classList.add("pjCard");
+            selectedCard.id = "Pj" + character.name;
+            // add Class for animation
+            selectedCard.classList.add("slide-top");
+            // Append to player deck
+            playerDeck.appendChild(selectedCard);
+        } else {
+            alert("You can only select up to 3 cards!");
+        }
+    });
+}
+function updateMainDeck(character){
+    //Removes available character from Main deck array
+    for(i=0;i<=charactersDeck.length;i++){
+        if(charactersDeck[i]===character){
+            charactersDeck.splice(i,1)
+        }
+    }
+}
+
+function ready()
+{
+    //Once the player is ready the remaining card will automatically go to the Pc deck
+    let pcDeck = document.getElementById("currentPCdeck");
+    for(i=0 ; i<charactersDeck.length ; i++) {
+        removeMainDeck(i);
+        let newPcCard = document.createElement("img");
+        newPcCard.src = charactersDeck[i].imgurl;
+        newPcCard.classList.add("pjCard");
+        newPcCard.id = "Pj" + charactersDeck[i].name;       
+        // Remove card from main deck
+        newPcCard.remove();
+        // Append to player deck       
+        pcDeck.appendChild(newPcCard);        
+    }
+    let section = document.getElementsByClassName('carddeck_Section')[0]; 
+    let msj = document.createElement("p");
+    msj.textContent="There is no mode cards! Ready to play? Click Start!";
+    msj.style.color="white";
+    section.appendChild(msj);
+
+}
+
+function removeMainDeck(i){
+    let cardId = "Pj" + charactersDeck[i].name;
+        let cardInMainDeck = document.getElementById(cardId); // Get the actual card in the main deck
+
+        if (cardInMainDeck) {
+            cardInMainDeck.remove(); // Remove from the main deck
+        }
+}
